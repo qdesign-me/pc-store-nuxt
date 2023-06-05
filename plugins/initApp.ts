9503f3185db6abf8190cbcbccaa8347776de54a5
Dzmitry Khiurenka <dzmitry@acst.com>
2023-06-05 10:23:28 +0300

    wip

--- plugins/initApp.ts
+++ plugins/initApp.ts
@@ -8,13 +8,10 @@ export default defineNuxtPlugin({
       console.log('app:created');
       const nuxtApp = useNuxtApp();
       const store = useAppStore(nuxtApp.$pinia);
-      if (process) {
-        const hostname = process?.env?.NUXT_PUBLIC_HOSTNAME;
-        if (hostname?.length) {
-          const initData = await fetch(`${hostname}/api/config`).then((res) => res.json());
-          store.config = initData.config;
-        }
-      }
+      const hostname = 'https://pc-store-nuxt.vercel.app';
+
+      const initData = await fetch(`${hostname}/api/config`).then((res) => res.json());
+      store.config = initData.config;
     },
   },
 });
