e37f30412e021f30d0f6739fdb0dc3328b0b7aad
Dzmitry Khiurenka <dzmitry@acst.com>
2023-10-02 11:43:38 +0300

    wip

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -14,30 +14,31 @@
       <Loading :class="{ 'opacity-0': !pending }" />
     </div>
 
-    <div class="mb-6" v-if="!pending && filters.q && products.total === 0">По запросу «{{ filters.q }}» ничего не найдено.</div>
-    <div class="mb-6" v-else-if="!pending && !filters.q && products.total === 0">{{ category.data.name }}: ничего не найдено.</div>
+    <div class="mb-6" v-if="!pending && filters.q && products?.total === 0">По запросу «{{ filters.q }}» ничего не найдено.</div>
+    <div class="mb-6" v-else-if="!pending && !filters.q && products?.total === 0">{{ category.data.name }}: ничего не найдено.</div>
 
-    <template v-if="!pending && products.total === 0">
+    <template v-if="!pending && products?.total === 0">
       <TopSalesBlock />
       <PopularCatsBlock />
       <NewItemsBlock />
     </template>
-    <template v-if="products.total > 0">
+    <template v-if="products?.total > 0">
       <div class="grid grid-cols-5 gap-2.5">
-        <div class="hidden xl:block" v-if="products.total > 0">
+        <div class="hidden xl:block" v-if="products?.total > 0">
           <Filters :blocks="category.blocks" />
         </div>
         <div class="col-span-5 xl:col-span-4">
-          <div class="text-[#E5A35B] mb-4">Всего {{ pluralize(products.total, ['товар', 'товара', 'товаров']) }}</div>
+          <div class="text-[#E5A35B] mb-4">Всего {{ pluralize(products?.total, ['товар', 'товара', 'товаров']) }}</div>
           <SortingLinks :uri="uri" />
           <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2.5 gap-y-8 products-grid">
-            <ProductCard v-for="product in products.data" :key="product.productID" :product="product" />
+            <ProductCard v-for="product in products?.data" :key="product.productID" :product="product" />
           </div>
-          <PaginationBox :uri="uri" :total="products.total" v-if="products.total > 12" />
+          <PaginationBox :uri="uri" :total="products?.total" v-if="products?.total > 12" />
         </div>
       </div>
     </template>
   </main>
+  =={{ category }}
 </template>
 <script async setup>
 const router = useRouter();
@@ -45,10 +46,6 @@ const filters = computed(() => router.currentRoute.value.query);
 
 provide('filters', filters);
 const uri = router.currentRoute.value.path;
-
-// if (1) {
-//   throw createError({ statusCode: 404, statusMessage: 'Page Not Found' });
-// }
 
 const { data: category } = await useFetch('/api/categories/one', {
   method: 'POST',
@@ -59,13 +56,16 @@ const { data: category } = await useFetch('/api/categories/one', {
 
 const { pending, data: products } = await useFetch('/api/categories/products', {
   lazy: true,
-
   method: 'POST',
   body: {
     uri,
     filters,
   },
 });
+
+if (!category?.value.data) {
+  throw createError({ statusCode: 404, statusMessage: 'Page Not Found' });
+}
 
 definePageMeta({
   layout: 'default',
