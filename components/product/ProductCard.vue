7fa891a5c1a53a483e3e4c861ab5f9394ea65c1c
Dzmitry Khiurenka <qdesign.by@gmail.com>
2023-11-05 17:44:05 +0300

    wip

--- components/product/ProductCard.vue
+++ components/product/ProductCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="card card-product border !pb-[115px] text-black">
-    <div class="tags min-h-[21px] -mb-3 absolute z-10">
+    <div class="tags min-h-[21px] absolute z-10">
       <div v-if="props.product.is_auction" class="bg-[#4DB732]">Аукцион</div>
       <div v-if="props.product.Price > 500" class="bg-[#F54D4D]">В рассрочку</div>
       <div v-if="props.product.PriceSale > 0" class="bg-[#FFAC2F]">На акции</div>
     </div>
-    <div>
+    <div class="pt-[30px]">
       <NuxtLink :to="uri" class="block">
         <ProductThumbs :data="props.product" />
       </NuxtLink>
