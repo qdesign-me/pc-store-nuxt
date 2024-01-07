d3d8e8bfb8bc1fc811902c7b755a92bc9a271519
Dzmitry Khiurenka <dzmitry@acst.com>
2024-01-07 17:03:56 +0300

    wip

--- db/db.ts
+++ db/db.ts
@@ -7,6 +7,13 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 });
 
 export default db;
